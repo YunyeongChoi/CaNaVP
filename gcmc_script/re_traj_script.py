08a484eebf0d746d8d702a7bccad680d7f6243c7
Yun Yeong Choi <yychoi94@berkeley.edu>
2022-10-10 12:04:07 -0700

    Using hdf5 now

--- gcmc_script/re_traj_script.py
+++ gcmc_script/re_traj_script.py
@@ -2,10 +2,9 @@ import os
 import json
 import numpy as np
 from glob import glob
-from smol.io import load_work
-from monty.serialization import dumpfn, loadfn
 from smol.cofe.space import Vacancy
 from pymatgen.core.sites import Species
+from smol.moca.sampler.container import SampleContainer
 
 
 def read_json(fjson):
@@ -37,10 +36,10 @@ def main():
 
     ca, na, v3, v4, v5, vac = species()
     json_save_dir = "/global/scratch/users/yychoi94/CaNaVP/gcmc_script/traj_data.json"
-    save_dir = "/global/scratch/users/yychoi94/CaNaVP_gcMC/data"
+    save_dir = "/global/scratch/users/yychoi94/CaNaVP_gcMC_1005/data"
     saved_list = glob(save_dir + "/*")
     # Currently have a problem on saving.
-    saved_list.remove("/global/scratch/users/yychoi94/CaNaVP_gcMC/data/-11.0_-2.0_cn_sgmc.mson")
+    # saved_list.remove("/global/scratch/users/yychoi94/CaNaVP_gcMC/data/-11.0_-2.0_cn_sgmc.mson")
 
     if os.path.exists(json_save_dir):
         result_dict = read_json(json_save_dir)
@@ -51,9 +50,10 @@ def main():
         print(end_list)
         name = '_'.join(end_list.split('/')[-1].split('_')[:2])
         if not name in result_dict.keys():
-            temp = loadfn(end_list)
+            temp = SampleContainer.from_hdf5(end_list)
             energy_set = temp.get_energies()
             species_count = temp.get_species_counts()
+            stop_i = len(energy_set)
             for i, j in enumerate(energy_set):
                 if (np.var(energy_set[i:i+1000])) < 0.1:
                     stop_i = i
@@ -65,6 +65,7 @@ def main():
             result_dict[name]['Ca_last'] = species_count[ca][-1]
             result_dict[name]['energy'] = list(energy_set[:stop_i])
             result_dict[name]['energy_last'] = energy_set[-1]
+            result_dict[name]['metadata'] = temp.metadata
 
         write_json(result_dict, json_save_dir)
 
